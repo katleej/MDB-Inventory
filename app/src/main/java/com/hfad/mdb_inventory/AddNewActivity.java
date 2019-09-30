c2157c52e8d7207ce38bddef38715760cb2ddbc3
Katniss Lee <jihyunlee@berkeley.edu>
2019-09-29 20:43:00 -0700

    search UI is fixed now

--- app/src/main/java/com/hfad/mdb_inventory/AddNewActivity.java
+++ app/src/main/java/com/hfad/mdb_inventory/AddNewActivity.java
@@ -81,6 +81,9 @@ public class AddNewActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
 
+        date.setFocusable(false);
+        date.setKeyListener(null);
+
     }
 
     @Override
